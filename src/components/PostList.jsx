54b3ece20aedc36aedb161eeb80efd5e84649c8c
jaewoni619 <108003438+jaewoni619@users.noreply.github.com>
2024-12-30 14:52:05 +0000

    sacing changes

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,18 @@
+import PostListItem from "./PostListItem"
+
+
 const PostList = () => {
     return(
-        <div className="">PostList</div>
+        <div className="flex flex-col gap-12 mb-8">
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+        </div>
     )
 }
 
---
+++ src/components/PostListItem.jsx
@@ -0,0 +1,10 @@
+const PostListItem = () => {
+    return(
+        <div className="flex flex-col xl:flex-row gap-8">
+            {/*image*/}
+            {/*details*/}
+        </div>
+    )
+}
+
+export default PostListItem
